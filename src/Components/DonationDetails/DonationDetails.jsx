ad0fd2a717551e5a2b047b950c04983b0960586c
Apollo <shixintan01@gmail.com>
2024-01-06 22:23:22 +0800

    Added donate button to donation page

--- src/Components/DonationDetails/DonationDetails.jsx
+++ src/Components/DonationDetails/DonationDetails.jsx
@@ -1,21 +1,44 @@
-import { useState } from "react"
-import classes from './DonationDetails.module.css'
+import { useState } from "react";
+import classes from "./DonationDetails.module.css";
 import DonationHistory from "./DonationHistory/DonationHistory";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const DonationDetails = () => {
-    const [popUp, setPopUp] = useState(false);
+  const [popUp, setPopUp] = useState(false);
 
-    const closeModal = () => {
-        setPopUp(false);
-    }
+  const openModal = () => {
+    setPopUp(true);
+  };
 
-    return (
-        <div>
-            <button className={classes.button} onClick={() => {setPopUp(true)}}>Donation History</button>
-            <h1>hey</h1>
-            {popUp && <DonationHistory closeModal={closeModal}/>}
-        </div>
-    )
-}
+  const closeModal = () => {
+    setPopUp(false);
+  };
 
-export default DonationDetails
+  const onDonateClicked = () => {
+    console.log("Redirecting to donatin page");
+  };
+  //#TODO: Centralised all the components
+  return (
+    <div>
+      <Stack
+        direction="row"
+        // divider={<Divider orientation="vertical" flexItem />}
+        justifyContent="space-around"
+        alignItems="center"
+        spacing={4}
+      >
+        <Button variant="contained" onClick={openModal}>
+          Donation History
+        </Button>
+        <Button size="large" variant="contained" onClick={onDonateClicked}>
+          Donate Now!!!
+        </Button>
+      </Stack>
+      <h1>hey</h1>
+      {popUp && <DonationHistory closeModal={closeModal} />}
+    </div>
+  );
+};
+
+export default DonationDetails;
