2b81587c69e7d9f3ca1595d221bede29e276736b
SuhailLoya <suhail.loya@gmail.com>
2024-01-08 21:05:40 +0800

    updated

---
+++ contracts/Test.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+
+contract Test {
+
+    address owner; //hospital
+    address[] donors;
+    uint256[] donations;
+    uint256 donorCount;
+
+    string public title; 
+    string public description;
+    uint256 public deadline; 
+    uint256 public amountCollected;
+    uint256 public amountNeeded;
+
+    event FundsSent(address indexed _owner);
+    event DonationReceived(address indexed _donor);
+
+    modifier validDonatedAmount(uint256 _amount) {
+        require(_amount <= amountNeeded, "Not a valid amount");
+        _;
+    }
+
+    constructor(
+        address _owner, 
+        string memory _title, 
+        string memory _description, 
+        uint256 _amountNeeded, 
+        uint256 _deadline
+    ) {
+        require(_deadline > block.timestamp, "The deadline should be a date in the future.");
+        owner = _owner;
+        title = _title;
+        description = _description;
+        amountNeeded = _amountNeeded;
+        deadline = _deadline;
+        amountCollected = 0;
+        donorCount = 0;
+    }
+
+    function sendFunds() private {
+        (bool sent,) = payable(owner).call{value: amountCollected}("");
+
+        if(sent) {
+            emit FundsSent(owner);
+        }
+    }
+
+    function returnFunds() private {
+        for (uint256 i = 0; i < donorCount; i++) {
+            (bool sent,) = payable(donors[i]).call{value: donations[i]}("");
+            require(sent, "Failed to refund");
+        }
+    }
+
+    function getDonors() view public returns (address[] memory, uint256[] memory) {
+        return (donors, donations);
+    }
+
+    receive() external payable validDonatedAmount(msg.value) {
+        uint256 amount = msg.value;
+
+        donors.push(msg.sender);
+        donations.push(amount);
+        donorCount++;
+
+        amountCollected += amount;
+        amountNeeded -= amount;
+
+        if (block.timestamp > deadline) {
+            if (donorCount > 0) {
+                returnFunds();
+                amountNeeded = 0;
+                amountCollected = 0;
+                donorCount = 0;
+            }
+        }
+
+        if (amountNeeded == 0) {
+            sendFunds();
+        } else {
+            emit DonationReceived(msg.sender);
+        }
+    }
+}
---
+++ createFundraiser.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const { Web3 } = require("web3");
+const fs = require("fs");
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// Connect to the EVM-compatible blockchain
+const web3 = new Web3("https://rpc-evm-sidechain.xrpl.org/");
+
+// Replace these with your contract's ABI and contract address
+const abiFilePath = "./contracts/output_directory/CareWalletFactory.abi";
+const contractABI = JSON.parse(fs.readFileSync(abiFilePath).toString());
+
+const contractAddress = "0x2a72cc1eac50d48d6d36dcb44627358e2a8bd4f1"; // Replace with your actual contract address
+
+const createFundraiser = async (
+    _owner,
+    _title,
+    _description,
+    _amountNeeded,
+    _deadline
+) => {
+    const myAccount = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
+    web3.eth.accounts.wallet.add(myAccount);
+    const fromAddress = myAccount.address;
+
+    console.log("Calling contract.create() from account:", fromAddress);
+
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+    // Assuming 'create' is a function in your smart contract
+    const createTx = contract.methods.create_CareWallet(
+        _owner,
+        _title,
+        _description,
+        _amountNeeded,
+        _deadline
+    );
+
+    const gas = await createTx.estimateGas({ from: fromAddress });
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const createTransaction = await web3.eth.accounts.signTransaction(
+        {
+            from: fromAddress,
+            to: contractAddress,
+            data: createTx.encodeABI(),
+            gas,
+            gasPrice,
+        },
+        PRIVATE_KEY
+    );
+
+    console.log("Sending transaction...");
+    const createReceipt = await web3.eth.sendSignedTransaction(
+        createTransaction.rawTransaction
+    );
+    console.log("Transaction hash:", createReceipt.transactionHash);
+};
+
+//createFundraiser().catch(console.error);
+module.exports = {
+    createFundraiser,
+};
