8dc68a36504e8a41e3e1c6cc4a8677662556b4bb
Apollo <shixintan01@gmail.com>
2024-01-08 21:37:22 +0800

    use Context successfully prepared

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,28 @@ import Chatbox from "./Components/NFTPage/Chatbox/Chatbox";
 import CreateDonation from "./Components/CreateDonation/CreateDonation";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MyProvider } from "./hooks/MyProvider";
+
 function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div>
-        <BrowserRouter>
-          <Navbar></Navbar>
-          <Routes>
-            <Route path="*" element={<Home />}></Route>
-            <Route path="/donate" element={<DonationList />}></Route>
-            <Route path="/donation/user1" element={<DonationDetails />}></Route>
-            <Route path="/create" element={<CreateDonation />}></Route>
-            <Route path="/mynft" element={<NFT />}></Route>
-            <Route path="/chat" element={<Chatbox />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <MyProvider>
+          <BrowserRouter>
+            <Navbar></Navbar>
+            <Routes>
+              <Route path="*" element={<Home />}></Route>
+              <Route path="/donate" element={<DonationList />}></Route>
+              <Route
+                path="/donation/user1"
+                element={<DonationDetails />}
+              ></Route>
+              <Route path="/create" element={<CreateDonation />}></Route>
+              <Route path="/mynft" element={<NFT />}></Route>
+              <Route path="/chat" element={<Chatbox />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </MyProvider>
       </div>
     </LocalizationProvider>
   );
