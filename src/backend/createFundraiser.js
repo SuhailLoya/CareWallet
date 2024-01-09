b533a8ca6d8eb175aade3597212f0a64e17c4fc3
zwe <zwezeya02@gmail.com>
2024-01-09 15:12:19 +0800

    bug fixes

--- src/backend/createFundraiser.js
+++ src/backend/createFundraiser.js
@@ -7,7 +7,7 @@ import { ethers } from "ethers";
 // const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY;
 
-import { contractABI } from "./contracts/artefacts";
+// import { contractABI } from "./contracts/artefacts";
 
 // Connect to the EVM-compatible blockchain
 const web3 = new Web3("https://rpc-evm-sidechain.xrpl.org/");
@@ -15,9 +15,9 @@ const web3 = new Web3("https://rpc-evm-sidechain.xrpl.org/");
 // Replace these with your contract's ABI and contract address
 // const abiFilePath = "./contracts/output_directory/CareWalletFactory.abi";
 
-// const contractABI = JSON.parse(readFileSync(abiFilePath).toString());
+const contractABI = JSON.parse(`[{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_description","type":"string"},{"internalType":"uint256","name":"_amountNeeded","type":"uint256"},{"internalType":"uint256","name":"_deadline","type":"uint256"}],"name":"create_CareWallet","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"fundraiserAddresses","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getNumberofCareWallets","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"get_CareWallet","outputs":[{"components":[{"internalType":"address","name":"id","type":"address"},{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint256","name":"amountCollected","type":"uint256"},{"internalType":"uint256","name":"amountNeeded","type":"uint256"}],"internalType":"struct CareWalletFactory.Fundraiser[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"listOf_CareWallets","outputs":[{"internalType":"contract Test","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"setNumberofCareWallets","outputs":[],"stateMutability":"nonpayable","type":"function"}]`);
 
-const contractAddress = "0x2a72cc1eac50d48d6d36dcb44627358e2a8bd4f1"; // Replace with your actual contract address
+const contractAddress = "0x3af49D8C77B252Ea9C3FA42e0D5A17ed67b8Bbf6"; // Replace with your actual contract address
 
 const createFundraiser = async (
   _owner,
--- src/backend/retrieveFundraisers.js
+++ src/backend/retrieveFundraisers.js
@@ -2,7 +2,7 @@
 // dotenv.config();
 import { Web3 } from "web3";
 // import { readFileSync } from "fs";
-import { contractABI } from "./contracts/artefacts";
+// import { contractABI } from "./contracts/artefacts";
 import { ethers } from "ethers";
 
 // const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -12,9 +12,9 @@ const web3 = new Web3("https://rpc-evm-sidechain.xrpl.org/");
 
 // Replace these with your contract's ABI and contract address
 // const abiFilePath = "./contracts/output_directory/CareWalletFactory.abi";
-// const contractABI = JSON.parse(readFileSync(abiFilePath));
+const contractABI = JSON.parse(`[{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_description","type":"string"},{"internalType":"uint256","name":"_amountNeeded","type":"uint256"},{"internalType":"uint256","name":"_deadline","type":"uint256"}],"name":"create_CareWallet","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"fundraiserAddresses","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getNumberofCareWallets","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"get_CareWallet","outputs":[{"components":[{"internalType":"address","name":"id","type":"address"},{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint256","name":"amountCollected","type":"uint256"},{"internalType":"uint256","name":"amountNeeded","type":"uint256"}],"internalType":"struct CareWalletFactory.Fundraiser[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"listOf_CareWallets","outputs":[{"internalType":"contract Test","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"setNumberofCareWallets","outputs":[],"stateMutability":"nonpayable","type":"function"}]`);
 
-const contractAddress = "0x2a72cc1eac50d48d6d36dcb44627358e2a8bd4f1"; // Replace with your actual contract address
+const contractAddress = "0x3af49D8C77B252Ea9C3FA42e0D5A17ed67b8Bbf6"; // Replace with your actual contract address
 
 const retrieveFundraisers = async () => {
   const myAccount = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
@@ -30,6 +30,7 @@ const retrieveFundraisers = async () => {
     .get_CareWallet()
     .call({ from: fromAddress });
   return result.map(r => {return {...r, amountNeeded: ethers.formatEther(r.amountNeeded), amountCollected: ethers.formatEther(r.amountCollected)}});
+  // return result
 };
 
 //retrieveFundraisers().catch(console.error);
