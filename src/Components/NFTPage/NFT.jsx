fb6b64ea89be07ad74dbe22089f05c649719cea7
ziyan <97217171+LingZiyann@users.noreply.github.com>
2024-01-18 00:07:48 +0800

    Added NFTInfo feature + error pop up when trying to donate beyond amount needed.

--- src/Components/NFTPage/NFT.jsx
+++ src/Components/NFTPage/NFT.jsx
@@ -3,30 +3,39 @@ import nft1 from "../../assets/nft1.png";
 import nft2 from "../../assets/nft2.png";
 import Chatbox from "./Chatbox/Chatbox";
 import { Helmet } from "react-helmet";
+import NFTList from "./NFTList/NFTList";
 
 const NFT = () => {
+  const [openNFT, setOpenNFT] = useState(false);
   const [openChat, setOpenChat] = useState(false);
 
-  const closeChat = () => {
-    setOpenChat(false);
+  const UseOpenChat = () => {
+    setOpenChat(true);
+  }
+
+  const closeNFT = () => {
+    setOpenNFT(false);
   };
+
+
 
   return (
     <div style={{ display: "flex" }}>
       <Helmet>
         <title>CareWallet - My NFTs</title>
       </Helmet>
-
-      {openChat && <Chatbox closeChat={closeChat} />}
+      {}
+      {openChat && <Chatbox closeChat={() => setOpenChat(false)}/>}
+      {openNFT && <NFTList closeNFT={closeNFT} PropsOpenChat={UseOpenChat}/>}
       <img
         src={nft2}
         style={{ width: "50%" }}
-        onClick={() => setOpenChat(true)}
+        onClick={() => setOpenNFT(true)}
       ></img>
       <img
         src={nft1}
         style={{ width: "50%" }}
-        onClick={() => setOpenChat(true)}
+        onClick={() => setOpenNFT(true)}
       ></img>
     </div>
   );
